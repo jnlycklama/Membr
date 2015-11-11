954ea29d456c791141d097999d3b9a7539eecd08
Julie Lycklama <jnlycklama@Julies-MacBook-Pro.local>
2015-11-11 17:32:43 -0500

    Initial commit - outline of first two pages

---
+++ app/src/main/java/com/example/jnlycklama/membr/SignUpFragment.java
@@ -0,0 +1,22 @@
+package com.example.jnlycklama.membr;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class SignUpFragment extends Fragment {
+
+    public SignUpFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_sign_up, container, false);
+    }
+}
